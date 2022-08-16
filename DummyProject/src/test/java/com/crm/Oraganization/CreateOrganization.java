6c60add553fee442d61fbd0cf400e5c620574e4a
Dhanamma6575 <dhanammamalaghan@gmail.com>
2022-08-16 16:08:09 +0530

    jenkins demo program

---
+++ DummyProject/src/test/java/com/crm/Oraganization/CreateOrganization.java
@@ -0,0 +1,33 @@
+package com.crm.Oraganization;
+
+
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class CreateOrganization {
+
+	@Test
+	public void method() throws Throwable  {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =new ChromeDriver();
+		
+		FileInputStream fis =new FileInputStream("./data.properties");
+		Properties plib =new Properties();
+		plib.load(fis);
+		String URL=plib.getProperty("url");
+		driver.get(URL);	
+		System.out.println("organization created");
+		
+
+	}
+
+}
